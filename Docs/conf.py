be39b3d6fef649741952e714251fbd7b5e1b3879
Allison Becker <beckerah@si.edu>
2020-06-04 14:32:22 -0500

    Update copyright
    Update date to 2020 and add Allison

--- Docs/conf.py
+++ Docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'SI Barcode Network Informatics Documentation'
-copyright = u'2016, Niamh Redmond, Mike Trizna'
+copyright = u'2020, Niamh Redmond, Mike Trizna, Allison Becker'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
